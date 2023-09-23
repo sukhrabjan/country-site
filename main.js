0b80c3497c0b2706efc4279f9f4bc0f01ea8248a
sukhrabjan <suhrobamonov550@gmail.com>
2023-09-23 13:57:00 +0500

    the country layout is finished

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const elDarkMode = document.querySelector(".header-button");
 
 elDarkMode.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
-  document.body.style.transition = '1s';
 });
 
 
